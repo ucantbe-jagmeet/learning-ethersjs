0f7e09ab87181868a65fa8be5410e702a04940e6
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-24 02:34:25 +0530

    showing current block number and changes ethers version

--- by-code-eater/readBlockchain.js
+++ by-code-eater/readBlockchain.js
@@ -1,1 +1,11 @@
 const { ethers } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://mainnet.infura.io/v3/d39bda9dc9734b43a9114723883f71be"
+);
+
+const queryBlockchain = async () => {
+  const block = await provider.getBlockNumber();
+  console.log("current block numner : ", block);
+};
+
+queryBlockchain();
